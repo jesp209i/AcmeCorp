071f5628f6dbe4d1d4461f44ff4db75a95ca694a
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-11-06 12:33:34 +0100

    Controller tests

---
+++ tests/WebAPI.Tests/CompetitionControllerTest.cs
@@ -0,0 +1,61 @@
+using AcmeCorp.Application.Commands;
+using AcmeCorp.WebApi.Controllers;
+using AutoFixture;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+
+namespace WebAPI.Tests
+{
+    public class CompetitionControllerTest
+    {
+        private readonly Mock<IMediator> _mediatorMock;
+        private readonly Fixture _fixture;
+        public CompetitionControllerTest()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _fixture = new Fixture();
+        }
+        [Fact]
+        public async Task EnterCompetition_ReturnsStatus200()
+        {
+            // Arrange
+            var expectedStatusCode = (int)HttpStatusCode.OK;
+            var request = _fixture.Create<EnterCompetition>();
+            var fakeResponse = _fixture.Create<EnterCompetitionResponse>();
+            _mediatorMock.Setup(x => x.Send(request, default)).ReturnsAsync(fakeResponse);
+            var controller = new CompetitionController(_mediatorMock.Object);
+            
+            // Act
+            var response = await controller.EnterCompetition(request);
+            var actualResponse = (OkObjectResult)response;
+
+            // Assert
+            actualResponse.StatusCode.Should().Be(expectedStatusCode);
+        }
+        [Fact]
+        public async Task EnterCompetition_MediatorSendsCorrectInput()
+        {
+            // Arrange
+            var request = _fixture.Create<EnterCompetition>();
+            EnterCompetition actual = null;
+            _mediatorMock.Setup(x => x.Send(request, default))
+                .Callback((IRequest<EnterCompetitionResponse> req, CancellationToken token) => actual = req as EnterCompetition)
+                .ReturnsAsync(It.IsAny<EnterCompetitionResponse>);
+            var controller = new CompetitionController(_mediatorMock.Object);
+
+            // Act
+            var response = await controller.EnterCompetition(request);
+
+            // Assert
+            actual.Should().Be(request);
+        }
+    }
+}
