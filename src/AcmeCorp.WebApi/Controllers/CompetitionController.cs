2057cd2c2da38ff20e7445f5a75fd17a6ae1d2b6
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-11-06 11:13:23 +0100

    Endpoint to enter competition added - some project example code removed

---
+++ src/AcmeCorp.WebApi/Controllers/CompetitionController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AcmeCorp.Application.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcmeCorp.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CompetitionController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public CompetitionController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async Task<IActionResult> EnterCompetition(EnterCompetition request)
+        {
+            return Ok(await _mediator.Send(request));
+        }
+    }
+}
