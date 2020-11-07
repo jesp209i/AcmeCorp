44c59d7a0b147cc5e18df1d25340a2b31473cc34
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-11-07 19:46:22 +0100

    Added first Migration

---
+++ src/AcmeCorp.Persistance/Migrations/20201107181907_AcmeInitialMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AcmeCorp.Persistance.Migrations
+{
+    public partial class AcmeInitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contestants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    SerialNumber = table.Column<string>(nullable: true),
+                    ConfirmedAgeRequirement = table.Column<bool>(nullable: false),
+                    AcceptedTerms = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contestants", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contestants");
+        }
+    }
+}
