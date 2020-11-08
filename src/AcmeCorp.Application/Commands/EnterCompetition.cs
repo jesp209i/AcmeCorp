7822ffa0dd6952de8889d80548eac26936d932d7
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-11-08 17:25:27 +0100

    EnterCompetitionHandler test+implementation

--- src/AcmeCorp.Application/Commands/EnterCompetition.cs
+++ src/AcmeCorp.Application/Commands/EnterCompetition.cs
@@ -1,23 +1,20 @@
-﻿using AcmeCorp.Infrastructure;
-using AcmeCorp.Persistance;
+﻿using AcmeCorp.Infrastructure.Interfaces;
+using AcmeCorp.Persistence;
+using AcmeCorp.Persistence.Models;
 using FluentValidation;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace AcmeCorp.Application.Commands
 {
-    public class EnterCompetition : IRequest<EnterCompetitionResponse>
+    public class EnterCompetition : IRequest
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public bool ConfirmsCorrectAge { get; set; }
         public string SerialNumber { get; set; }
+        public bool ConfirmsCorrectAge { get; set; }
         public bool AcceptsTerms { get; set; }
     }
     public class EnterCompetitionValidator : AbstractValidator<EnterCompetition>
@@ -29,27 +26,33 @@ namespace AcmeCorp.Application.Commands
         {
             _productService = productService;
             _competitionRepo = competitionRepo;
+            RuleFor(x => x.FirstName).NotEmpty().WithMessage("You must write you first name.");
+            RuleFor(x => x.LastName).NotEmpty().WithMessage("You must write you last name.");
             RuleFor(x => x.ConfirmsCorrectAge).Equal(true).WithMessage("You must be 18 to enter.");
             RuleFor(x => x.AcceptsTerms).Equal(true).WithMessage("You must accept the terms and condition to enter.");
             RuleFor(x => x.Email).EmailAddress().WithMessage("You must provide a valid email address, for us to contact you in the case you win the draw.");
             RuleFor(x => x.SerialNumber)
-                .Must(_productService.IsSerialNumberValid).WithMessage("You must provide a valid serial number to enter competition.")
+                .MustAsync(async (x, _) => { return await _productService.IsSerialNumberValid(x); }).WithMessage("You must provide a valid serial number to enter competition.")
                 .MustAsync(async (x, _) => { 
                     bool eligibleSerialNumber = await _competitionRepo.IsSerialNumberEligible(x); 
                     return eligibleSerialNumber; 
                 }).WithMessage("Serial number is no longer eligiable to enter draw.");
+            
         }
     }
-    public class EnterCompetitionHandler : IRequestHandler<EnterCompetition, EnterCompetitionResponse>
+    public class EnterCompetitionHandler : IRequestHandler<EnterCompetition>
     {
-        public Task<EnterCompetitionResponse> Handle(EnterCompetition request, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-    }
+        private readonly ICompetitionRepository _competitionRepo;
 
-    public class EnterCompetitionResponse
-    {
-        public bool EntryAccepted { get; set; }
+        public EnterCompetitionHandler(ICompetitionRepository competitionRepository)
+        {
+            _competitionRepo = competitionRepository;
+        }
+        public async Task<Unit> Handle(EnterCompetition request, CancellationToken cancellationToken = default)
+        {
+            var contestant = Contestant.Create(request.FirstName, request.LastName, request.Email, request.SerialNumber, request.ConfirmsCorrectAge, request.AcceptsTerms);
+            await _competitionRepo.AddContestant(contestant);
+            return new Unit();
+        }
     }
 }
---
+++ tests/Application.Tests/Handlers/EnterCompetitionHandlerTest.cs
@@ -0,0 +1,42 @@
+﻿using AcmeCorp.Application.Commands;
+using AcmeCorp.Persistence;
+using AcmeCorp.Persistence.Models;
+using AutoFixture;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.Tests.Handlers
+{
+    public class EnterCompetitionHandlerTest
+    { 
+        private readonly Fixture _fixture;
+
+        public EnterCompetitionHandlerTest()
+        {
+            _fixture = new Fixture();
+        }
+        [Fact]
+        public async Task EnterCompetition_ContestantRelayedThrough()
+        {
+            // Arrange
+            Contestant actualContestant = null;
+            var request = _fixture.Create<EnterCompetition>();
+            var expectedContestant = Contestant.Create(request.FirstName, request.LastName, request.Email, request.SerialNumber, request.ConfirmsCorrectAge, request.AcceptsTerms);
+            var repoMock = new Mock<ICompetitionRepository>();
+            repoMock.Setup(x => x.AddContestant(It.IsAny<Contestant>()))
+                .Callback((Contestant contestant) => actualContestant = contestant).Returns(Task.CompletedTask);
+            var handler = new EnterCompetitionHandler(repoMock.Object);
+
+            // Act
+            var response = await handler.Handle(request);
+
+            // Assert
+            actualContestant
+                .Should().BeEquivalentTo(expectedContestant, options => options.Excluding(x=>x.CreatedAt));
+            actualContestant.CreatedAt.Should().BeCloseTo(expectedContestant.CreatedAt, 1.Seconds());
+        }
+    }
+}
