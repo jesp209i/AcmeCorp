9eba853f399a997facf2d469081e5e00413c3b86
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-11-10 19:34:20 +0100

    form to enter competition, including handling of validations errors from backend

--- src/acmecorp/src/components/Competition.vue
+++ src/acmecorp/src/components/Competition.vue
@@ -1,19 +1,88 @@
 <template>
     <div class="competition">
         <h1>Enter competition!</h1>
-        <p>Win prizes - be wild!</p>
-        <form id="competition">
-        <table>
+        <p>Win prizes - Just enter a qualifying serial number, and you will get two entries in the draw.</p>
         
-            <tr><td>First Name:</td><td> <input type="text" v-model="formdata.firstName" /></td></tr>
-            <tr><td>Last Name:</td><td> <input type="text" v-model="formdata.lastName" /></td></tr>
-            <tr><td>Email:</td><td> <input type="text" v-model="formdata.email" /></td></tr>
-            <tr><td>Serial Number:</td><td> <input type="text" v-model="formdata.serialNumber" /></td></tr>
-            <tr><td>Confirm you are over 18 years old</td><td> <input type="checkbox" v-model="formdata.confirmsCorrectAge" /></td></tr>
-            <tr><td>Accept terms and conditions</td><td> <input type="checkbox" v-model="formdata.acceptsTerms" /></td></tr>
-            <tr><td></td><td><button v-on:click.prevent="sendForm">Send</button></td></tr>
-        </table></form>
-        <pre>{{errors}}</pre>
+        
+        <div v-if="success" class="ui success icon message" :class="{'hidden': !success}">
+          <i class="inbox icon"></i>
+          <i class="close icon" v-on:click="toggleSuccess()"></i>
+          <div class="content">
+          <div class="header">Congratulations!</div>
+          <p>You have entered the competition!</p>
+          </div>
+        </div>
+        
+        
+        <form v-if="!success" id="competition" :class="['ui','form',{'error': errors}]">
+        <div :class="['field', {'error': errorResponse.SerialNumber}]">
+            <label>Serial Number:</label>
+            <input type="text" v-model="formdata.serialNumber" />
+        </div>
+        <div class="ui error message" :class="{'hidden': !errorResponse.SerialNumber}">
+          <div class="header">Check the Serial Number</div>
+          <ul>
+            <li v-for="(err, idx) in errorResponse.SerialNumber" :key="idx">{{ err}}</li>
+          </ul>
+        </div>
+        <div :class="['field', {'error': errorResponse.FirstName}]">
+            <label>First Name:</label>
+            <input type="text" v-model="formdata.firstName" />
+        </div>
+                <div class="ui error message" :class="{'hidden': !errorResponse.FirstName}">
+          <div class="header">Error in First Name Field</div>
+          <ul>
+            <li v-for="(err, idx) in errorResponse.FirstName" :key="idx">{{ err}}</li>
+          </ul>
+        </div>
+        <div :class="['field', {'error': errorResponse.LastName}]">
+          <label>Last Name:</label>
+          <input type="text" v-model="formdata.lastName" />
+        </div>
+                <div class="ui error message" :class="{'hidden': !errorResponse.LastName}">
+          <div class="header">Error in Last Name Field</div>
+          <ul>
+            <li v-for="(err, idx) in errorResponse.LastName" :key="idx">{{ err}}</li>
+          </ul>
+        </div>
+        <div :class="['field', {'error': errorResponse.Email}]">
+          <label>Email:</label>
+          <input type="text" v-model="formdata.email" />
+        </div>
+                <div class="ui error message" :class="{'hidden': !errorResponse.Email}">
+          <div class="header">Error in Email Field</div>
+          <ul>
+            <li v-for="(err, idx) in errorResponse.Email" :key="idx">{{ err}}</li>
+          </ul>
+        </div>
+        <div class="two fields">
+        <div :class="['field', {'error': errorResponse.ConfirmsCorrectAge}]">
+          <div class="ui checkbox" >
+            <input type="checkbox" v-model="formdata.confirmsCorrectAge" />
+          <label>Confirm you are over 18 years old</label>
+          </div>
+        </div>
+          <div :class="['field', {'error': errorResponse.AcceptsTerms}]">
+            <div class="ui checkbox">
+              <input type="checkbox" v-model="formdata.acceptsTerms" />
+              <label>Accept terms and conditions</label>
+            </div>
+          </div>
+        </div>
+        <div class="ui error message" :class="{'hidden': !errorResponse.ConfirmsCorrectAge}">
+          <ul>
+            <li v-for="(err, idx) in errorResponse.ConfirmsCorrectAge" :key="idx">{{ err}}</li>
+          </ul>
+        </div>
+        <div class="ui error message" :class="{'hidden': !errorResponse.AcceptsTerms}">
+          <ul>
+            <li v-for="(err, idx) in errorResponse.AcceptsTerms" :key="idx">{{ err}}</li>
+          </ul>
+        </div>
+        <button type="submit" class="ui blue right labeled icon button" v-on:click.prevent="sendForm">
+          <i class="right arrow icon"></i>
+          Send</button>
+        </form>
     </div>
 </template>
 <script>
@@ -32,21 +101,41 @@ export default {
                 confirmsCorrectAge: false,
                 acceptsTerms : false
             },
-            errors :[]
+            success: true,
+            errors :false,
+            errorResponse: []
         }
     },
     methods: {
         async sendForm(){
-            console.log(this.formdata)
+            this.errors = []
             try {
-                await axios.post(
-                sampleConfig.resourceServer.messagesUrl,
-                this.formdata
+                var response = await axios.post(
+                  sampleConfig.resourceServer.competitionUrl,
+                  this.formdata
                 )
+                if (response.status === 200){
+                    this.errors = false
+                    this.errorResponse = []
+                    this.success = true
+                    this.clearForm()
+                }
             } catch (e) {
-                this.errors = e.response.data.errors
-                console.log(e.response)
+                this.errorResponse = e.response.data.errors
+                this.errors = true;
+                this.success = false;
             }
+        },
+        clearForm() {
+            this.formdata.firstName = ''
+            this.formdata.lastName = ''
+            this.formdata.email = ''
+            this.formdata.serialNumber = ''
+            this.formdata.confirmsCorrectAge = false
+            this.formdata.acceptsTerms = false
+        },
+        toggleSuccess(){
+          this.success = !this.success
         }
     }
 }
